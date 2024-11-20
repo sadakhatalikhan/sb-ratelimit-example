50f2b370e836e1f6c9ccc77b2feb0b7bac4d6b09
Sadakhat ali khan Navab <sadakhatalikhannavab@Sadakhats-MacBook-Air.local>
2024-11-20 08:12:46 +0530

    Initial commit

---
+++ src/main/java/com/sb/ratelimit/sb_ratelimit_example/SbRatelimitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sb.ratelimit.sb_ratelimit_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRatelimitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRatelimitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/ratelimit/sb_ratelimit_example/SbRatelimitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.ratelimit.sb_ratelimit_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRatelimitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
